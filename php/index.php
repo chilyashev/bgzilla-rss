ef2b6ac5bb5b1d0aa9330b84665708db2a0fd284
Mihail Chilyashev <chilyashev@gmail.com>
2014-07-08 13:07:09 +0300

    Added install button

--- php/index.php
+++ php/index.php
@@ -1,3 +1,31 @@
-<?
-	header("Location: app.html");
+<!doctype html>
+<html lang="bg-BG">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <script type="text/javascript">
+        function installApp(){
+            var manifestUrl = 'http://board.chilyashev.com/dev/openshift/bgzillarss/php/manifest.webapp';
+            var request = window.navigator.mozApps.install(manifestUrl);
+            request.onsuccess = function () {
+                // Save the App object that is returned
+                var appRecord = this.result;
+                alert("Application installed");
+            };
+            request.onerror = function () {
+                // Display the error information from the DOMError object
+                alert('Installation error:' + this.error.name);
+            };
+        }
+    </script>
+</head>
+<body>
+<div id="install">
+	<a href="javascript:" onclick="installApp();">
+	<img src="app-icons/icon-128.png" /><br/>
+	Install
+	</a>
+</div>
+</body>
+</html>
 
